0b48f80ce3cf835c91c24a1c088ab603d6a02392
Crawfish-h <jaidendcrawford@gmail.com>
2025-10-08 09:38:32 -0400

    Negative numbers are now working better. No longer printing trailing zeros.

--- Calculator_Loop.cpp
+++ Calculator_Loop.cpp
@@ -7,11 +7,33 @@
 #include "Match.h"
 #include "Util.h"
 
+// Returns true if any trailing zeros were removed.
+bool Remove_Trailing_Zeros(std::string& number_String)
+{
+    bool removed_Zero = false;
+    for (ssize_t i = number_String.size() - 1; i > -1; i--)
+    {
+        if (number_String[i] == '0')
+        {
+            number_String.erase(number_String.begin() + i);
+            removed_Zero = true;
+        }else
+        {
+            break;
+        }
+    }
+
+    return removed_Zero;
+}
+
 bool Is_Digit(const std::string& string)
 {
-    for (char ch : string)
+    for (size_t i = 0; i < string.size(); i++)
     {
-        if (isdigit(ch) == false && ch != '.')
+        if (i == 0 && string[i] == '-')
+            continue;
+
+        if (isdigit(string[i]) == false && string[i] != '.')
         {
             return false;
         }
@@ -38,56 +60,65 @@ bool Find_Value(const std::vector<std::string>& tokens, size_t i, bool forwards 
     return false;
 }
 
+bool Eval_Basic_Op(size_t i, std::vector<std::string>& tokens)
+{
+    bool do_Basic_Op = false;
+
+    if (Eq_Multi<Or>(tokens[i], "+", "-", "*", "/"))
+    {
+        do_Basic_Op = true;
+    }
+
+    if (do_Basic_Op == true)
+    {
+        if (i == 0 || Is_Digit(tokens[i - 1]) == false)
+        {
+            Print("tokens: {}\n", Is_Digit(tokens[i - 1]) == false);
+            tokens.clear();
+            Print("Error: expected number left of operator.\n");
+            return false;
+        }else if (i == tokens.size() - 1 || Is_Digit(tokens[i + 1]) == false)
+        {
+            tokens.clear();
+            Print("Error: expected number right of operator.\n");
+            return false;
+        }else
+        {
+            double value = 0.0;
+            double left_Val = std::stod(tokens[i - 1]);
+            double right_Val = std::stod(tokens[i + 1]);
+
+            Match(tokens[i])
+            .Case("+", [&]{ value = left_Val + right_Val; })
+            .Case("-", [&]{ value = left_Val - right_Val; })
+            .Case("*", [&]{ value = left_Val * right_Val; })
+            .Case("/", [&]{ value = left_Val / right_Val; })
+            .Default([]
+            { 
+                Print("Error: expected basic operator: +, -, *, or /.\n");
+                return false;
+            });
+
+            tokens[i + 1] = std::to_string(value);
+            tokens.erase(tokens.begin() + i);
+            tokens.erase(tokens.begin() + i - 1);
+        }
+    }
+
+    return true;
+}
+
 void Eval_Tokens(std::vector<std::string>& tokens)
 {
     while (tokens.size() > 1)
     {
         for (size_t i = 0; i < tokens.size(); i++)
         {
-            bool do_Basic_Op = false;
-
-            if (Eq_Multi<Or>(tokens[i], "+", "-", "*", "/"))
-            {
-                do_Basic_Op = true;
-            }
-
-            if (do_Basic_Op == true)
-            {
-                if (i == 0 || Is_Digit(tokens[i - 1]) == false)
-                {
-                    tokens.clear();
-                    Print("Error: expected number left of operator.\n");
-                    return;
-                }else if (i == tokens.size() - 1 || Is_Digit(tokens[i + 1]) == false)
-                {
-                    tokens.clear();
-                    Print("Error: expected number right of operator.\n");
-                    return;
-                }else
-                {
-                    double value = 0.0;
-                    double left_Val = std::stod(tokens[i - 1]);
-                    double right_Val = std::stod(tokens[i + 1]);
-
-                    Match(tokens[i])
-                    .Case("+", [&]{ value = left_Val + right_Val; })
-                    .Case("-", [&]{ value = left_Val - right_Val; })
-                    .Case("*", [&]{ value = left_Val * right_Val; })
-                    .Case("/", [&]{ value = left_Val / right_Val; })
-                    .Default([]
-                    { 
-                        Print("Error: invalid input.\n");
-                        return;
-                    });
-
-                    tokens[i + 1] = std::to_string(value);
-                    tokens.erase(tokens.begin() + i);
-                    tokens.erase(tokens.begin() + i - 1);
-                }
-            }
+            if (not Eval_Basic_Op(i, tokens)) return;
         }
     }
 
+    Remove_Trailing_Zeros(tokens[0]);
     Print("{}\n", tokens[0]);
     tokens.clear();
 }
