bb37d8f1a16bd0f69bc4c9147eb8645fd4766b8d
Crawfish-h <jaidendcrawford@gmail.com>
2025-10-09 14:18:08 -0400

    Added funtionality to the evaluation of parentheses.

--- Calculator_Loop.cpp
+++ Calculator_Loop.cpp
@@ -1,46 +1,12 @@
 #include <iostream>
 #include <string>
-#include <cctype>
+#include <cmath>
 #include <vector>
 #include <type_traits>
+#include <ranges>
 #include "Print.h"
 #include "Match.h"
 #include "Util.h"
-
-// Returns true if any trailing zeros were removed.
-bool Remove_Trailing_Zeros(std::string& number_String)
-{
-    bool removed_Zero = false;
-    for (ssize_t i = number_String.size() - 1; i > -1; i--)
-    {
-        if (number_String[i] == '0')
-        {
-            number_String.erase(number_String.begin() + i);
-            removed_Zero = true;
-        }else
-        {
-            break;
-        }
-    }
-
-    return removed_Zero;
-}
-
-bool Is_Digit(const std::string& string)
-{
-    for (size_t i = 0; i < string.size(); i++)
-    {
-        if (i == 0 && string[i] == '-')
-            continue;
-
-        if (isdigit(string[i]) == false && string[i] != '.')
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
 
 bool Find_Value(const std::vector<std::string>& tokens, size_t i, bool forwards = true)
 {
@@ -64,7 +30,7 @@ bool Eval_Basic_Op(size_t i, std::vector<std::string>& tokens)
 {
     bool do_Basic_Op = false;
 
-    if (Eq_Multi<Or>(tokens[i], "+", "-", "*", "/"))
+    if (Eq_Multi<Or>(tokens[i], "+", "-", "*", "/", "%", "pw"))
     {
         do_Basic_Op = true;
     }
@@ -85,17 +51,34 @@ bool Eval_Basic_Op(size_t i, std::vector<std::string>& tokens)
         }else
         {
             double value = 0.0;
-            double left_Val = std::stod(tokens[i - 1]);
-            double right_Val = std::stod(tokens[i + 1]);
+            double left_Val = 0.0;
+            double right_Val = 0.0;
+
+            try
+            {
+                left_Val = std::stod(tokens[i - 1]);
+                right_Val = std::stod(tokens[i + 1]);
+            }catch(const std::invalid_argument& ia)
+            {
+                Print("Exeception thrown: 'std::invalid_argument'\n");
+                Print("left_Val as string: {}\n", tokens[i - 1]);
+                Print("right_Val as string: {}\n", tokens[i + 1]);
+                Print("tokens: ");
+                Print("{}\n", tokens);
+                return false;
+            }
+            
 
             Match(tokens[i])
             .Case("+", [&]{ value = left_Val + right_Val; })
             .Case("-", [&]{ value = left_Val - right_Val; })
             .Case("*", [&]{ value = left_Val * right_Val; })
             .Case("/", [&]{ value = left_Val / right_Val; })
+            .Case("%", [&]{ value = std::fmod(left_Val, right_Val); })
+            .Case("pw", [&]{ value = std::pow(left_Val, right_Val); })
             .Default([]
             { 
-                Print("Error: expected basic operator: +, -, *, or /.\n");
+                Print("Error: expected basic operator: +, -, *, /, %, or pw.\n");
                 return false;
             });
 
@@ -108,13 +91,70 @@ bool Eval_Basic_Op(size_t i, std::vector<std::string>& tokens)
     return true;
 }
 
+bool Eval_Parens(std::vector<std::string>& tokens, bool& has_Parens)
+{
+    std::vector<size_t> right_Parens_Pos;
+    std::vector<size_t> left_Parens_Pos;
+    for (size_t i = 0; i < tokens.size(); i++)
+    {
+        if (tokens[i] == "(")
+            left_Parens_Pos.push_back(i);
+        else if (tokens[i] == ")")
+            right_Parens_Pos.push_back(i);
+    }
+
+    if (left_Parens_Pos.size() != right_Parens_Pos.size())
+    {
+        Print("Error: number of left parentheses, {}, does not match number of right parentheses, {}.", 
+            left_Parens_Pos.size(), right_Parens_Pos.size());
+
+        return false;
+    }
+
+    while (right_Parens_Pos.size() > 0)
+    {
+        for (size_t i : left_Parens_Pos | std::views::reverse)
+        {
+            for (size_t j = i + 1; j < right_Parens_Pos[0] && j < tokens.size(); j++)
+            {
+                if (tokens[j] == ")")
+                    break;
+
+                Eval_Basic_Op(j, tokens);
+            }
+
+            tokens.erase(tokens.begin() + i);
+            tokens.erase(tokens.begin() + right_Parens_Pos[0]);
+            right_Parens_Pos.erase(right_Parens_Pos.begin());
+            left_Parens_Pos.erase(left_Parens_Pos.begin() + i);
+        }
+
+        Print("right_Parens_Pos: ");
+        Print("{}\n", right_Parens_Pos);
+    }
+
+    has_Parens = false;
+    return true;
+}
+
 void Eval_Tokens(std::vector<std::string>& tokens)
 {
+    // These booleans are used to implement PEMDAS.
+    bool has_Parens = true;
+    bool has_Exponents = true;
+    bool has_Mul_Div = true;
+    bool has_Add_Sub = true;
     while (tokens.size() > 1)
     {
         for (size_t i = 0; i < tokens.size(); i++)
         {
-            if (not Eval_Basic_Op(i, tokens)) return;
+            if (has_Parens)
+            {
+                if (not Eval_Parens(tokens, has_Parens)) return;
+            }else
+            {
+                if (not Eval_Basic_Op(i, tokens)) return;
+            }
         }
     }
 
@@ -138,16 +178,20 @@ void Calculator_Loop()
         std::getline(std::cin, input);
         std::string input_Scanned = "";
 
-        for (char chai : input)
+        for (size_t i = 0; i < input.size(); i++)
         {
-            switch(chai)
+            if (i != input.size() - 1 && input[i] == 'p' && input[i + 1] == 'w')
+            {
+                tokens.push_back(std::string("") + input[i] + input[i + 1]);
+            }
+
+            switch(input[i])
             {
                 case '+':
                 case '-':
                 case '*':
                 case '/':
                 case '%':
-                case '^':
                 case '(':
                 case ')':
                 case '<':
@@ -157,10 +201,11 @@ void Calculator_Loop()
                         tokens.push_back(input_Scanned);
                     input_Scanned = "";
 
-                    tokens.push_back(std::string("") + chai);
+                    tokens.push_back(std::string("") + input[i]);
                     break;
                 default:
-                    input_Scanned += chai;
+                    if (input[i] != ' ')
+                        input_Scanned += input[i];
                     break;
             }
         }
--- Util.h
+++ Util.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <array>
 
 enum Comp { And, Or };
 
@@ -9,4 +10,39 @@ bool Eq_Multi(T&& value, Args&&... args)
         return ((value == args) && ...);
     else
         return ((value == args) || ...);
+}
+
+// Returns true if any trailing zeros were removed.
+bool Remove_Trailing_Zeros(std::string& number_String)
+{
+    bool removed_Zero = false;
+    for (ssize_t i = number_String.size() - 1; i > -1; i--)
+    {
+        if (number_String[i] == '0')
+        {
+            number_String.erase(number_String.begin() + i);
+            removed_Zero = true;
+        }else
+        {
+            break;
+        }
+    }
+
+    return removed_Zero;
+}
+
+bool Is_Digit(const std::string& string)
+{
+    for (size_t i = 0; i < string.size(); i++)
+    {
+        if (i == 0 && string[i] == '-')
+            continue;
+
+        if (isdigit(string[i]) == false && string[i] != '.')
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
